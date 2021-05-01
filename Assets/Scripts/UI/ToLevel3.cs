30eab09f166699373cdfec4592a66fe7c1e8eb02
jshour <zenthon127@gmail.com>
2021-04-30 17:41:02 -0700

    Added UI to level 1 (porting to others)

---
+++ Assets/Scripts/UI/ToLevel3.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLevel3 : MonoBehaviour
+{
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
---
+++ Assets/Scripts/UI/ToLevel4.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLevel4 : MonoBehaviour
+{
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+}
